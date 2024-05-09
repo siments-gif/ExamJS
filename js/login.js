89a58efddc752c6af0a102bc9e7343a9c9c876df
Simen Skogen <simen.skogen@gokstadakademiet.no>
2024-05-09 14:19:13 +0200

    Made script ref as module type. Added buttons, title  and globalStyling refrences

--- js/login.js
+++ js/login.js
@@ -1,9 +1,14 @@
+import { styleButtons, titleStyles } from "./globalStyling.js";
+
 // Creating login elements (HTML structure)
 const loginSection = document.createElement("section");
 loginSection.id = "loginSection";
 
 const loginForm = document.createElement("form");
 loginForm.id = "loginForm";
+
+const formTitle = document.createElement("h1");
+formTitle.textContent = "Login Form"
 
 const userNameLabel = document.createElement("label");
 userNameLabel.id = "userNameLabel";
@@ -21,12 +26,29 @@ const passwordInput = document.createElement("input");
 passwordInput.id = "passwordInput";
 passwordInput.type = "password";
 
+const loginButton = document.createElement("button");
+loginButton.id = "loginBtn";
+loginButton.textContent = "Login";
+
+const register = document.getElementById("goToRegister")
+register.innerHTML = `
+    <p>Already have an account? <a href="../registerPage.html">Register here</a></p>
+`
+
 // Appending elements to DOM with right parent/child relationship
 document.body.appendChild(loginSection);
 loginSection.appendChild(loginForm);
+loginForm.appendChild(formTitle);
 
 loginForm.appendChild(userNameLabel);
 loginForm.appendChild(userNameInput);
 
 loginForm.appendChild(passwordLabel);
 loginForm.appendChild(passwordInput);
+
+loginForm.appendChild(loginButton);
+loginForm.appendChild(register);
+
+// Global Style functions
+styleButtons();
+titleStyles();
