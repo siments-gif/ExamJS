cdc0b63493dd8b38a5abed0d2261c5f1cd99aeeb
Simen Skogen <simen.skogen@gokstadakademiet.no>
2024-05-14 12:56:14 +0200

    Added clickHandling for save and goToCollection. Made the buttons work like they should. Changed name on collection html

--- js/detailPage.js
+++ js/detailPage.js
@@ -1,6 +1,7 @@
 import { fetchPokemonDetails } from "./apiFetch.js"; // Importing fetch Function
 import { styleButtons, bodyStyle, titleStyles } from "./globalStyling.js";
 
+// Creating every page element
 const pageTitle = document.createElement("h1")
 pageTitle.textContent = "Pokemon Details"
 pageTitle.style.textAlign = "center";
@@ -36,6 +37,7 @@ cardSection.style.margin = "0 auto"
 cardSection.style.border = "3px solid black"
 cardSection.style.borderRadius = "3rem"
 
+// Symboles for matching some types
 const symbols = {
     "light": "🌟",
     "fire": "🔥",
@@ -44,6 +46,30 @@ const symbols = {
     "grass": "🌿",
     "poison": "☢"
 };
+
+// Function for handling eventListener
+function saveBtnHandler(pokemon) {
+    let uploadData = {
+        name: pokemon.name,
+        frontImage: pokemon.frontImage,
+        backImage: pokemon.backImage,
+        weight: pokemon.weight,
+        types: pokemon.types,
+        abilities: pokemon.abilities
+    }
+    let existingData = JSON.parse(localStorage.getItem("pokemonCollection")) || [];
+        
+        // Checking if existing data is an array
+        if(!Array.isArray(existingData)) {
+            existingData = []; // If it isnt an array, make it one
+        }
+        
+        console.log(uploadData); // To check what is getting uploaded inside array of data
+        existingData.push(uploadData);
+        console.log(existingData); // Checking whats in the collection from before with new saved addition
+        localStorage.setItem("pokemonCollection", JSON.stringify(existingData));
+        alert(`${pokemon.name} saved to your collection`);
+}
         
 async function displayPokemonDetails() {
     try {
@@ -54,34 +80,33 @@ async function displayPokemonDetails() {
         const symbolForTypes = pokemon.types.map(type => symbols[type]); // Maps symbols to match type and symbol
 
         pokemonCard.innerHTML = `
-            <h2>${pokemon.name}</h2>
-            <div id="detailImages">
-                <img src="${pokemon.frontImage}"></img>
-                <img src="${pokemon.backImage}"></img>
-            </div>
-            <p>Weight: ${pokemon.weight} kg</p>
-            <p>Types: ${pokemon.types.join(' , ')} -${symbolForTypes.join(', ')}</p> <!-- Adding and splitting by comma -->
-            <p>Abilities: ${pokemon.abilities.join(' , ')}</p>
+                <h2>${pokemon.name}</h2>
+                <div id="detailImages">
+                    <img src="${pokemon.frontImage}"></img>
+                    <img src="${pokemon.backImage}"></img>
+                </div>
+                <p>Weight: ${pokemon.weight} kg</p>
+                <p>Types: ${pokemon.types.join(' , ')} -${symbolForTypes.join(', ')}</p> <!-- Adding and splitting by comma -->
+                <p>Abilities: ${pokemon.abilities.join(' , ')}</p>
             `;
         pokemonCard.querySelector("#detailImages").style.display = "flex"; // Styling image div inside of innerhtml
         pokemonCard.style.display = "flex";
         pokemonCard.style.flexDirection = "column";
         pokemonCard.style.alignItems = "center";
         pokemonCard.style.justifyContent = "center";
-        
         titleStyles(); // Reference to a global style function
+
+        saveBtn.addEventListener("click", function(){
+            saveBtnHandler(pokemon) // Refrencing our handler to work inside the card
+        });
+        collectionBtn.addEventListener("click", function(){
+            location.href = "../collectionPage.html";
+        });
+        
     } catch (error) {
         throw new Error("Didn't display any pokemon details on page", error);
     }
 }
-
-saveBtn.addEventListener("click", function(e) {
-    e.preventDefault();
-
-    const uploadData = JSON.parse(localStorage.getItem("pokemonCollection")) || [];
-    uploadData.push(displayPokemonDetails(pokemonCard))
-    localStorage.setItem("pokemonCollection", JSON.stringify(uploadData));
-})
 displayPokemonDetails(); // Puts the display function on page
 styleButtons(); // Reference to a global style function
 bodyStyle(); // Reference to a global style function
