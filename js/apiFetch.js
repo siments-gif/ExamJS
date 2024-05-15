3653d910e2a056dd8a74be9d8d8a93c494ec5a9e
Simen Skogen <simen.skogen@gokstadakademiet.no>
2024-05-15 11:26:40 +0200

    Found out fix for error code 400 with saving to backendAPI

--- js/apiFetch.js
+++ js/apiFetch.js
@@ -13,25 +13,7 @@ export async function fetchBaseData (endpoint) {
     }
 }
 
-export async function saveToCollection(uploadData) {
-    try {
-        const res = await fetch(`https://crudapi.co.uk/api/v1/collection`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                    Authorization: "Bearer U42Uxh60_bv0V7KTbbLFs18ys9Tsu7_N-NS9woLWx41GtGq8-A",
-            },
-            body: JSON.stringify(uploadData)
-        })
-        if(!res.ok) {
-            throw new Error(`Something went wrong with ${res.status}`);
-        }
-        const data = await res.json();
-        console.log(data);
-    } catch (error) {
-        throw new Error("Something went wrong with posting details to collection", error)
-    }
-}
+
 
 export async function fetchPokemonDetails(pokemonId) {
     try {
@@ -57,6 +39,25 @@ export async function fetchPokemonDetails(pokemonId) {
     }
 }
 
+export async function saveToCollection(uploadData) {
+    try {
+        const res = await fetch(`https://crudapi.co.uk/api/v1/collection`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                    Authorization: "Bearer U42Uxh60_bv0V7KTbbLFs18ys9Tsu7_N-NS9woLWx41GtGq8-A",
+            },
+            body: JSON.stringify([uploadData])
+        })
+        if(!res.ok) {
+            throw new Error(`Something went wrong with ${res.status}`);
+        }
+        const data = await res.json();
+        console.log(data);
+    } catch (error) {
+        throw new Error("Something went wrong with posting details to collection", error)
+    }
+}
 export async function registerUser(username, password, email, phone) {
     try {
         const res = await fetch("https://crudapi.co.uk/api/v1/user", {
